cc88735972c2a2eb3a1feac7d8046a409012536c
Jon Knebel <jonknebel@gmail.com>
2025-04-22 11:03:58 +0800

    fixed an error with icons on web

--- lib/icons/iconWithClassName.ts
+++ lib/icons/iconWithClassName.ts
@@ -1,7 +1,24 @@
 import type { LucideIcon } from "lucide-react-native";
 import { cssInterop } from "nativewind";
+import { Platform } from "react-native";
 
 import { cn } from "../utils";
+
+// List of responder props that cause warnings on web
+const RESPONDER_PROPS = [
+    "onResponderTerminate",
+    "onResponderTerminationRequest",
+    "onStartShouldSetResponder",
+    "onStartShouldSetResponderCapture",
+    "onMoveShouldSetResponder",
+    "onMoveShouldSetResponderCapture",
+    "onResponderGrant",
+    "onResponderReject",
+    "onResponderMove",
+    "onResponderRelease",
+    "onResponderEnd",
+    "onResponderStart",
+];
 
 export function iconWithClassName(icon: LucideIcon) {
     cssInterop(icon, {
@@ -16,6 +33,16 @@ export function iconWithClassName(icon: LucideIcon) {
     // Wrap icon to set default className
     const wrappedIcon = (props: any) => {
         const mergedProps = { className: cn("text-foreground", props.className), ...props };
+
+        // Filter out responder props when running on web
+        if (Platform.OS === "web") {
+            RESPONDER_PROPS.forEach(prop => {
+                if (prop in mergedProps) {
+                    delete mergedProps[prop];
+                }
+            });
+        }
+
         return icon(mergedProps);
     };
     return wrappedIcon as LucideIcon;
