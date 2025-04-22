73ff0ca1c1373a80994dc85cd93cffbd158f9119
Jon Knebel <jonknebel@gmail.com>
2025-04-22 11:12:57 +0800

    fixed an error with icons on web

--- lib/icons/iconWithClassName.ts
+++ lib/icons/iconWithClassName.ts
@@ -4,22 +4,6 @@ import { Platform } from "react-native";
 
 import { cn } from "../utils";
 
-// List of responder props that cause warnings on web
-const RESPONDER_PROPS = [
-    "onResponderTerminate",
-    "onResponderTerminationRequest",
-    "onStartShouldSetResponder",
-    "onStartShouldSetResponderCapture",
-    "onMoveShouldSetResponder",
-    "onMoveShouldSetResponderCapture",
-    "onResponderGrant",
-    "onResponderReject",
-    "onResponderMove",
-    "onResponderRelease",
-    "onResponderEnd",
-    "onResponderStart",
-];
-
 export function iconWithClassName(icon: LucideIcon) {
     cssInterop(icon, {
         className: {
@@ -30,20 +14,53 @@ export function iconWithClassName(icon: LucideIcon) {
             },
         },
     });
+
     // Wrap icon to set default className
     const wrappedIcon = (props: any) => {
-        const mergedProps = { className: cn("text-foreground", props.className), ...props };
-
-        // Filter out responder props when running on web
+        // For web platform, extract only the props we need
+        // This avoids passing unknown props like onResponderTerminate to DOM
         if (Platform.OS === "web") {
-            RESPONDER_PROPS.forEach(prop => {
-                if (prop in mergedProps) {
-                    delete mergedProps[prop];
-                }
+            const {
+                className,
+                color,
+                size,
+                stroke,
+                strokeWidth,
+                style,
+                fill,
+                // Explicitly omit responder props that cause warnings
+                onResponderTerminate,
+                onResponderTerminationRequest,
+                onStartShouldSetResponder,
+                onStartShouldSetResponderCapture,
+                onMoveShouldSetResponder,
+                onMoveShouldSetResponderCapture,
+                onResponderGrant,
+                onResponderReject,
+                onResponderMove,
+                onResponderRelease,
+                onResponderEnd,
+                onResponderStart,
+                ...restProps
+            } = props;
+
+            // Only pass the props that web SVG components need
+            return icon({
+                className: cn("text-foreground", className),
+                color,
+                size,
+                stroke,
+                strokeWidth,
+                style,
+                fill,
+                ...restProps
             });
         }
 
+        // For native platforms, pass all props through
+        const mergedProps = { className: cn("text-foreground", props.className), ...props };
         return icon(mergedProps);
     };
+
     return wrappedIcon as LucideIcon;
 }
